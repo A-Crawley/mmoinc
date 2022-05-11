12f0a816e366f7ab75d6a10e1ff7254c2fa6b238
A-Crawley <crawleyandrew3@gmail.com>
2022-05-11 21:35:32 +1000

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Button, Container, Paper, Typography } from "@mui/material";
+import Fruit from "./Fruit.js";
+import { areArraysEqual } from "@mui/base";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const init = async () => {
+  if (supabase.auth.user() === undefined) return;
+
+  const { data: inventories, error } = await supabase
+    .from("inventories")
+    .select("*")
+    .eq("user_id", supabase.auth.user().id);
+
+  console.log({ inventory: inventories[0] });
+
+  return { inventory: inventories[0] };
+};
+
+export default function App() {
+  const [session, setSession] = useState(null);
+  const [inventory, setInventory] = useState({ fruit: { amount: 0 } });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true)
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    init().then((e) => {
+      console.log({fruit: e.inventory.fruit, e})
+      setInventory({
+        ...inventory,
+        fruit: e.inventory.fruit,
+      });
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container maxWidth="lx">
+        <Paper elevation={2} sx={{ height: "600px", marginTop: "50px" }}>
+          {
+            loading 
+            ? null
+            : <Fruit pickSpeed={500} fruit={inventory.fruit.amount} />
+          }
+        </Paper>
+        {!session ? <Auth /> : null}
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
